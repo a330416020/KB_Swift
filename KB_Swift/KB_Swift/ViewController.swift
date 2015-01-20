a8dc41b1fc4cde4527b951bebb56b3afcdf40838
leihuiwu <330416020@qq.com>
2015-01-20 15:34:19 +0800

    new project

---
+++ KB_Swift/KB_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KB_Swift
+//
+//  Created by leihuiwu on 15/1/20.
+//  Copyright (c) 2015年 leihuiwu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
